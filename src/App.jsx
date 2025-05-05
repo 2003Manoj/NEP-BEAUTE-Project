745c9a23e92390a368ad5be77faeca92d7c98b13
2003Manoj <manojaryal322@gmail.com>
2025-05-05 18:52:04 +0545

    changes in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,6 @@ import WishlistPage from "./pages/WishlistPage/WishlistPage"
 import ProfilePage from "./pages/ProfilePage/ProfilePage"
 import OrdersPage from "./pages/OrdersPage/OrdersPage"
 import QuickViewPage from "./pages/QuickViewPage/QuickViewPage"
-
 import { AuthProvider } from "./contexts/AuthContext"
 import { CartProvider } from "./contexts/CartContext"
 import { WishlistProvider } from "./contexts/WishlistContext"
